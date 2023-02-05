7d47a484f7a3918679cc56869027b438ebd2d246
atulrananavare <rananavareatul07@gmail.com>
2023-02-05 12:45:50 +0530

    Feb5 code daily

---
+++ src/main/java/com/daily/Feb/feb5/AlphabetCheck.java
@@ -0,0 +1,13 @@
+package com.daily.Feb.feb5;
+
+public class AlphabetCheck {
+    public static void main(String[] args) {
+
+        char ch = 'Z';
+
+        if( ch>='a' && ch<='z'){
+            System.out.println("lowercase letter");
+        }else
+        System.out.println("uppercase letter");
+    }
+}
