6a0e0b076ac289ad0205e08c352b1cfcdfc4922d
atulrananavare <rananavareatul07@gmail.com>
2023-02-07 11:40:58 +0530

    Feb7 code daily

--- src/main/java/com/daily/Feb/feb7/MaximumNumOfWordsInSentence.java
+++ src/main/java/com/daily/Feb/feb7/MaximumNumOfWordsInSentence.java
@@ -7,11 +7,15 @@ package com.daily.Feb.feb7;
 //        - The third sentence, "this is great thanks very much", has 6 words in total.
 //        Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.
 
+import java.util.Arrays;
+
 public class MaximumNumOfWordsInSentence {
 
     public static void main(String[] args) {
         String[] arr = {"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
         System.out.println(mostWordsFound(arr));
+        System.out.println("===========================================================================");
+        System.out.println(maxWords(arr));
     }
 
     public static int mostWordsFound(String[] sentences) {
@@ -22,5 +26,12 @@ public class MaximumNumOfWordsInSentence {
         }
         return maxWords;
 
+    }
+
+    public static int maxWords(String[] sentences){
+        return Arrays.stream(sentences)
+                .map(sentence -> sentence.split(" ").length)
+                .max(Integer::compareTo)
+                .orElse(0);
     }
 }
