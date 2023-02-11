8b24636f34b7a152b5e1542b2eae233d78fd972b
atulrananavare <rananavareatul07@gmail.com>
2023-02-11 18:26:37 +0530

    Feb11 code daily

---
+++ src/main/java/com/daily/Feb/feb11/IntersectionOfArrays.java
@@ -0,0 +1,16 @@
+package com.daily.Feb.feb11;
+
+import java.util.Arrays;
+
+public class IntersectionOfArrays {
+    public static void main(String[] args) {
+        int[] arr1={4,9,5};
+        int[] arr2={9,4,9,8,4};
+        intersection(arr1, arr2);
+
+    }
+
+    public static void intersection(int[] arr1,int[] arr2){
+        Arrays.stream(arr1).filter(num -> Arrays.stream(arr2).anyMatch(y -> y == num)).distinct().forEach(System.out::println);
+    }
+}
