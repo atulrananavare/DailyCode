4342321eb2a31bccc53eeb2491470af7320a8a95
atulrananavare <rananavareatul07@gmail.com>
2023-02-11 17:51:44 +0530

    Feb11 code daily

---
+++ src/main/java/com/daily/Feb/feb11/MaxInArrays.java
@@ -0,0 +1,32 @@
+package com.daily.Feb.feb11;
+
+public class MaxInArrays {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 15, 4, 6,78,98,788,90};
+
+        System.out.println(maxInArray(arr));
+        System.out.println("===============================");
+        System.out.println(maxInrange(arr,1,6));
+    }
+
+    private static int maxInArray(int[] arr) {
+        int maxVal = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+
+    //Range max
+    private static int maxInrange(int[] arr,int start,int end) {
+        int maxVal = arr[start];
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+}
